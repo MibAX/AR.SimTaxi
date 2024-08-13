ffdee91a56d960fbe0df97b84b8663afbca94bd2
MibAX <154632432+MibAX@users.noreply.github.com>
2024-08-13 21:19:54 +0300

    Refactor Car controller and Index and Details Views (Pages)

--- AR.SimTaxi/Controllers/CarsController.cs
+++ AR.SimTaxi/Controllers/CarsController.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using AR.SimTaxi.Data;
 using AR.SimTaxi.Data.Entities;
@@ -12,30 +7,40 @@ namespace AR.SimTaxi.Controllers
 {
     public class CarsController : Controller
     {
+        #region Data and Const
+
         private readonly ApplicationDbContext _context;
 
         public CarsController(ApplicationDbContext context)
         {
             _context = context;
-        }
+        } 
 
-        // GET: Cars
+        #endregion
+
+        #region Actions
+
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Cars.ToListAsync());
+            var cars = await _context
+                                .Cars
+                                .ToListAsync();
+
+            return View(cars);
         }
 
-        // GET: Cars/Details/5
-        public async Task<IActionResult> Details(int? id)
+        public async Task<IActionResult> Details(int? id) 
         {
             if (id == null)
             {
                 return NotFound();
             }
 
-            var car = await _context.Cars
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (car == null)
+            var car = await _context
+                                .Cars
+                                .FirstOrDefaultAsync(m => m.Id == id); // lambda expression
+
+            if (car == null) // No car in the DB with id if found
             {
                 return NotFound();
             }
@@ -43,15 +48,11 @@ namespace AR.SimTaxi.Controllers
             return View(car);
         }
 
-        // GET: Cars/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Cars/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Model,Color,PlateNumber,ProductionYear,CarType,PowerType")] Car car)
@@ -65,7 +66,6 @@ namespace AR.SimTaxi.Controllers
             return View(car);
         }
 
-        // GET: Cars/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -81,9 +81,6 @@ namespace AR.SimTaxi.Controllers
             return View(car);
         }
 
-        // POST: Cars/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Model,Color,PlateNumber,ProductionYear,CarType,PowerType")] Car car)
@@ -116,7 +113,6 @@ namespace AR.SimTaxi.Controllers
             return View(car);
         }
 
-        // GET: Cars/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -134,7 +130,6 @@ namespace AR.SimTaxi.Controllers
             return View(car);
         }
 
-        // POST: Cars/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
@@ -149,9 +144,14 @@ namespace AR.SimTaxi.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        #endregion
+
+        #region Private Methods
+
         private bool CarExists(int id)
         {
             return _context.Cars.Any(e => e.Id == id);
-        }
+        } 
+        #endregion
     }
 }
