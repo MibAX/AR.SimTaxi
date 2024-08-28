09d243e448a8ed71a3ee98c2b4de6a7b55a319b5
MibAX <154632432+MibAX@users.noreply.github.com>
2024-08-28 20:51:08 +0300

    - Added Passenger Entity - Added DbSet<Passenger> Passengers - Added Migration Passegners table and email nullable

--- AR.SimTaxi/Data/ApplicationDbContext.cs
+++ AR.SimTaxi/Data/ApplicationDbContext.cs
@@ -8,6 +8,7 @@ namespace AR.SimTaxi.Data
     {
         public DbSet<Car> Cars { get; set; }
         public DbSet<Driver> Drivers { get; set; }
+        public DbSet<Passenger> Passengers { get; set; }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
---
+++ AR.SimTaxi/Data/Entities/Passenger.cs
@@ -0,0 +1,42 @@
+﻿using AR.SimTaxi.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AR.SimTaxi.Data.Entities
+{
+    public class Passenger
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public string? Email { get; set; }
+        public Gender? Gender { get; set; }
+
+
+        [NotMapped]
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+
+        [NotMapped]
+        public int? Age 
+        {
+            get
+            {
+                if(DateOfBirth.HasValue)
+                {
+                    return DateTime.Now.Year - DateOfBirth.Value.Year;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
